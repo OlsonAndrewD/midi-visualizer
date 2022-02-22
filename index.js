67c8bbb48993ab2223b6136cda9629691da70c09
Andrew Olson <olson.andrew.d@gmail.com>
2022-02-22 01:12:36 -0600

    Add some progress logging to console

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const config = {
     height: 360
 }
 
+console.log('Parsing MIDI file...')
 const song = parseMidi(config.midiFileName)
+
+console.log(`Laying out frames for ${song.notes.length} notes...`)
 const frames = layoutFrames({config, song})
+
+console.log(`Generating ${frames.length} frame images...`)
 const imageDirectory = generateImages(frames, config.width, config.height)
 // createVideo(imageDirectory, config.fps, config.outputFileName)
+
+console.log('All done!')
