563c9b550b5133c6d973ede75040e02dc2ca49e1
Andrew Olson <olson.andrew.d@gmail.com>
2022-02-28 21:52:12 -0600

    Fix the bug fix.
    I didn't realize that the custom interpreter was also responsible for advancing the pointer by doing the read operation, but it makes sense that it should have to.

--- parseMidi.js
+++ parseMidi.js
@@ -4,9 +4,10 @@ const {readFileSync} = require("fs")
 // There seems to be a bug in the midi-parser-js code that handles meta events.
 // It reads twice as many data bytes as it should, effectively causing it to skip data
 // that shouldn't be read as part of the unknown event.
-// But if we give it a customInterpreter to invoke for unknown events, it reads the correct number of bytes.
-// So, here's a customInterpreter that just ignores the data that was read.
+// But if we give it a customInterpreter to invoke for unknown events, we can make it read the correct number of bytes.
+// So, here's a customInterpreter that just reads it but ignores the result.
 midiParser.customInterpreter = (msgType, arrayBuffer, metaEventLength) => {
+    arrayBuffer.readInt(metaEventLength)
     console.log(`returning 0 for data of unknown msgType ${msgType} of length ${metaEventLength}`)
     return 0
 }
