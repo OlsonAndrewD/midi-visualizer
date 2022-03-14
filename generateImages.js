2438655cc06580654b8bce26a9192e7b167efafd
Andrew Olson <olson.andrew.d@gmail.com>
2022-03-13 19:21:00 -0500

    Put logging back in.

--- generateImages.js
+++ generateImages.js
@@ -202,7 +202,7 @@ module.exports = (frames, width = 480, height = 360) => {
         drawFrame(ctx, frame)
         var b64 = canvas.toDataURL("image/png").split("data:image/png;base64,")[1]
         writeFileSync(`./frames/frame${('' + i).padStart(7, '0')}.png`, b64, "base64")
-        // console.log(`\x1b[1A\x1b[2K\rDone with ${i}/${frames.length} frames...`)
+        console.log(`\x1b[1A\x1b[2K\rDone with ${i}/${frames.length} frames...`)
     })
     console.log("\x1b[1A\x1b[2K\r\x1b[1A")
     return "frames"
