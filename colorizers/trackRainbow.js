59c5444d540a4662ff74f35cc2dedf6119c16f1c
J-Meow <j@jmeow.net>
2022-03-21 20:43:46 -0500

    Add startingTrack input to trackRainbow colorizer

--- colorizers/trackRainbow.js
+++ colorizers/trackRainbow.js
@@ -1,3 +1,3 @@
-module.exports = ({colors = ["red", "orange", "yellow", "#00ff00", "blue", "indigo", "violet"]}) => ({track}) => {
-    return colors[track % colors.length]
+module.exports = ({colors = ["red", "orange", "yellow", "#00ff00", "blue", "indigo", "violet"], startingTrack = 0}) => ({track}) => {
+    return colors[Math.abs(track - startingTrack) % colors.length]
 }
