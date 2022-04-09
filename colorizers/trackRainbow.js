66890e8f955ed104d2cfd4735f9ac8620816f788
Andrew Olson <olson.andrew.d@gmail.com>
2022-04-09 16:50:31 -0500

    Fix bug in trackRainbow colorizer.

--- colorizers/trackRainbow.js
+++ colorizers/trackRainbow.js
@@ -2,6 +2,6 @@ module.exports = ({
     colors = ["red", "orange", "yellow", "#00ff00", "blue", "indigo", "violet"],
     startingTrack = 0
 }) => {
-    const palette = colors.map(c => { fillStyle: c })
+    const palette = colors.map(c => ({ fillStyle: c }))
     return ({ track }) => palette[Math.abs(track - startingTrack) % colors.length]
 }
